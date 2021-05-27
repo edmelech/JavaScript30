6561f464ef7731efea0538d8b44df9b10e7da3d0
Ed Melech <mr.melech@gmail.com>
2021-05-27 12:02:50 +0100

    select player and video

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,7 @@
+// get our elements //
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+
+// build our functions //
+
+// Hook up the event listeners //
